b4cb7e6c0bf1205dade6ab88cc35667373334ef1
Mike Brown <michael.brown@motimatic.com>
2025-03-05 21:25:08 -0800

    Axios setup to pass cookies

--- src/shared/utils/http-clients/djangoHttpClient.ts
+++ src/shared/utils/http-clients/djangoHttpClient.ts
@@ -2,7 +2,10 @@ import axios from 'axios'
 import djangoRequestInterceptor from "../interceptors/djangoRequestInterceptor";
 
 
-const httpClient = axios.create();
+const httpClient = axios.create({
+                                                withCredentials: true // ✅ Ensure cookies are sent with each request);
+                                              }
+                                            );
 
 const [requestInterceptor, errorInterceptor] = djangoRequestInterceptor();
 
