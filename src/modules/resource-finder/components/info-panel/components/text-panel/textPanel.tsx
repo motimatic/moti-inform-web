9338c4d255dc835da80bb5d9336b5bcadd4e136e
Jesus Balderrama <jesus.balderrama@motimatic.com>
2025-03-10 11:46:17 -0600

    change in metric panel and text panel to make it dynamic

--- src/modules/resource-finder/components/info-panel/components/text-panel/textPanel.tsx
+++ src/modules/resource-finder/components/info-panel/components/text-panel/textPanel.tsx
@@ -1,11 +1,20 @@
+import { useSnapshot } from "valtio";
+import { resourceFinderStore } from "../../../../../../state/resourceFinderStore";
+import { Title, Text } from "@mantine/core";
 
 
 const TextPanel = () => {
-
-
+    useSnapshot(resourceFinderStore);
+    const currentPage = resourceFinderStore.context.getCurrentPage();
+    const summary = currentPage?.summary;
+    if (!summary) return null;
     return (
-        <div style={{paddingTop:'10px', paddingBottom:'10px'}}>
-            Text Panel
+        <div className="py-10 px-6">
+            <Title order={3}>{summary?.pre || ""}</Title>
+            { summary.value != null &&
+                <Title order={2}  className="border-b-3 w-20 border-yellow-500 rounded-x">{summary?.value}</Title>
+            }
+            <Text size="lg" mt={10}>{summary?.post != "" && `${summary?.post},`} {summary?.prompt || ""}</Text>
         </div>
     );
 };
